07f739b135a134c478d185711371c6707add9734
mincheol-shin <mincheol-shin@naver.com>
2019-09-18 23:40:01 +0900

    Updated 1 solution

---
+++ Python/08. Errors and Exceptions/Exceptions/solution.py
@@ -0,0 +1,12 @@
+T = int(input())
+
+for _ in range(T):
+    try:
+        a, b = map(int,input().split())
+        print("{}".format(int(a/b)))
+    except ZeroDivisionError as e:
+        print("Error Code: integer division or modulo by zero")
+    except ValueError as e:
+        print("Error Code: {}".format(e))
+
+
