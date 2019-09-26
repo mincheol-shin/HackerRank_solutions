237542384fad4e1d76133c8e60788ef3e6cd2883
mincheol-shin <mincheol-shin@naver.com>
2019-09-26 11:42:08 +0900

    Delete File

--- 30 Days of Code/Day 00 - Hello, World/solution.py
+++
@@ -1,4 +0,0 @@
-
-input_string = input()
-print('Hello, World.\n{}'.format(input_string))
-
--- 30 Days of Code/Day 01 - Data Types/solution.py
+++
@@ -1,7 +0,0 @@
-i = 4
-d = 4.0
-s = 'HackerRank '
-
-
-
-print("{0}\n{1}\n{2}".format(int(input())+i, float(input())+d, s+input()))
