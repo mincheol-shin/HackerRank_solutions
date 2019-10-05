d5839ab03e652a697916972230e924779b66a343
mincheol-shin <mincheol-shin@naver.com>
2019-10-05 10:12:02 +0900

    Updated 1 solution

---
+++ 30 Days of Code/Day 13 - Abstract Classes/solution.py
@@ -0,0 +1,31 @@
+from abc import ABCMeta, abstractmethod
+
+class Book(object, metaclass=ABCMeta):
+
+    def __init__(self,title,author):
+        self.title=title
+        self.author=author
+
+    @abstractmethod
+    def display(self):
+        pass
+
+class MyBook(Book):
+
+    __slots__ = ["price"]
+
+    def __init__(self,title, author, price):
+        super().__init__(title, author)
+        self.price = price
+
+    def display(self):
+        print("Title: {}".format(title))
+        print("Author: {}".format(author))
+        print("Price: {}".format(price))
+
+
+title=input()
+author=input()
+price=int(input())
+new_novel=MyBook(title,author,price)
+new_novel.display()
