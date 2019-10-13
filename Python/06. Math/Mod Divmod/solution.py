09c2770db7c823d25d570a4130f8ef8c6a2c8e28
mincheol-shin <mincheol-shin@naver.com>
2019-10-13 19:52:18 +0900

    Updated 1 soluton

---
+++ Python/06. Math/Mod Divmod/solution.py
@@ -0,0 +1,8 @@
+if __name__ == "__main__":
+
+    a, b = int(input()), int(input())
+
+    # a를 b로 나눈 몫과 나머지를 리턴
+    result = divmod(a,b)
+
+    print("{0}\n{1}\n{2}".format(result[0],result[1],result))
