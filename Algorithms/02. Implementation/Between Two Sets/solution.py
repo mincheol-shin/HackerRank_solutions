4e0cc9f2c13ba420aab671c15953cb72767cadc4
mincheol-shin <mincheol-shin@naver.com>
2019-11-08 16:26:58 +0900

    add comment

--- Algorithms/02. Implementation/Between Two Sets/solution.py
+++ Algorithms/02. Implementation/Between Two Sets/solution.py
@@ -2,7 +2,7 @@ def getTotalX(a, b):
     result = 0
 
     for i in range(min(a), min(b) + 1, min(a)):
-        if all(i % a_element == 0 for a_element in a) and all(b_element % i == 0for b_element in b):
+        if all(i % a_element == 0 for a_element in a) and all(b_element % i == 0 for b_element in b):
             result += 1
     return result
 
