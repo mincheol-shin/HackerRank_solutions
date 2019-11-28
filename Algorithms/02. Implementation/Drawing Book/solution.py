4db691f9c27591fbbfdb08020074402ed6813480
mincheol-shin <mincheol-shin@naver.com>
2019-11-28 11:24:41 +0900

    Updated 1 solution

--- Algorithms/02. Implementation/Drawing Book/solution.py
+++ Algorithms/02. Implementation/Drawing Book/solution.py
@@ -1,24 +1,29 @@
 def pageCount(n, p):
-    book = [1, ]
-    count = []
+    book = [[1], ]
+    count = 0
+    reversed_count = 0
     for i in range(2, n, 2):
-        book.append([i, i+1])
+        book.append([i, i + 1])
 
     if n % 2 == 0:
-        book.append(n)
+        book.append([n])
 
     for i in range(len(book)):
-        temp = 0
-        for j in range(i+1, len(book)):
-            pass
-    print(count)
+        if p in book[i]:
+           break
+        count += 1
+
+    for i in range(-1, -len(book)-1, -1):
+        if p in book[i]:
+           break
+        reversed_count += 1
+
+
+    return reversed_count if count > reversed_count else count
+
+
 if __name__ == '__main__':
     n = int(input())
-
     p = int(input())
-
     result = pageCount(n, p)
-
     print("{}".format(result))
-
-
