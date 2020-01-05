a7b2a546fb1592a4db936800ec43736b117a72c8
ShinMinCheol <mincheol-shin@naver.com>
2020-01-05 14:22:22 +0900

    Terminated due to timeout

--- Algorithms/02. Implementation/Climbing the Leaderboard  /solution.py
+++ Algorithms/02. Implementation/Climbing the Leaderboard  /solution.py
@@ -1,11 +1,19 @@
 if __name__ == "__main__":
     players = int(input())
-    leader_board = list(set(map(int, input().split())))
+    leader_board = list(map(int, input().split()))
     alice_plays = int(input())
     alice_scores = list(map(int, input().split()))
-    count = 0
+    # 정렬 알고리즘 사용시 복잡도가 너무 커져서 Test case 6 ~ 10 통과 불가
+    # 사용자의 입력이 정렬된 상태임을 파악
+
     for i in alice_scores:
-        leader_board.append(i)
-        leader_board.sort()
-        leader_board.reverse()
-        print(leader_board.index(i)+1)
+        count = 0
+        for j in range(len(leader_board)):
+            if j > 0:
+                if leader_board[j] == leader_board[j-1]:
+                    count +=1
+            if i >= leader_board[j]:
+                print("{0}".format(j+1-count))
+                break
+            if j+1 == len(leader_board):
+                print("{}".format((j+2)-count))
