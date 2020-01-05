f67d682db60591719943692789027f06596dc7a8
ShinMinCheol <mincheol-shin@naver.com>
2020-01-05 15:16:52 +0900

    passed one test case

--- Algorithms/02. Implementation/Climbing the Leaderboard  /solution.py
+++ Algorithms/02. Implementation/Climbing the Leaderboard  /solution.py
@@ -1,19 +1,16 @@
+from collections import Counter
+
 if __name__ == "__main__":
     players = int(input())
-    leader_board = list(map(int, input().split()))
+    leader_board = list(Counter(map(int, input().split())).keys())
     alice_plays = int(input())
     alice_scores = list(map(int, input().split()))
     # 정렬 알고리즘 사용시 복잡도가 너무 커져서 Test case 6 ~ 10 통과 불가
-    # 사용자의 입력이 정렬된 상태임을 파악
-
+    # leader board에 대한 사용자 입력은 내림차순, alice score에 대한 사용자 입력은 오름차순
     for i in alice_scores:
-        count = 0
-        for j in range(len(leader_board)):
-            if j > 0:
-                if leader_board[j] == leader_board[j-1]:
-                    count +=1
-            if i >= leader_board[j]:
-                print("{0}".format(j+1-count))
+        for j in range(len(leader_board)-1, -1, -1):
+            if leader_board[j] > i:
+                print("{}".format(j+2))
                 break
-            if j+1 == len(leader_board):
-                print("{}".format((j+2)-count))
+            elif j == 0:
+                print("{}".format('1'))
